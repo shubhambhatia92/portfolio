d793d2ba2fed97b80a7b7c11bb1539f058b9db59
shubham <shubhambhatia92@gmail.com>
2018-10-23 12:43:16 +0530

    final commit

--- blog/models.py
+++ blog/models.py
@@ -7,4 +7,10 @@ class blog(models.Model):
     body=models.TextField()
     image=models.ImageField(upload_to='images/')
     date=models.DateTimeField()
-
+    def summary(self):
+        return self.body[:100]
+    def date_pretty(self):
+        return self.date.strftime('%b %e %Y')
+    def __str__(self):
+        return self.title
+        
